04b06e10e9939b5f3267bd84cd3c6204abdc63c0
unknown <puschkin0@web.de>
2015-01-05 11:56:03 +0100

    Host und Client für die GameActivity erstellt

---
+++ src/com/github/mrm1st3r/cards/ingame/Gameclient.java
@@ -0,0 +1,14 @@
+package com.github.mrm1st3r.cards.ingame;
+
+import android.os.Bundle;
+
+import com.github.mrm1st3r.cards.R;
+
+public class Gameclient extends GameActivity{
+
+	@Override
+	protected void onCreate(Bundle bun) {
+		super.onCreate(bun);
+		setContentView(R.layout.activity_game);
+	}
+}
---
+++ src/com/github/mrm1st3r/cards/ingame/Gamemaster.java
@@ -0,0 +1,22 @@
+package com.github.mrm1st3r.cards.ingame;
+
+import android.os.Bundle;
+
+import com.github.mrm1st3r.cards.R;
+import com.github.mrm1st3r.cards.game.*;
+
+public class Gamemaster extends GameActivity {
+	
+	int max = 0;
+
+	@Override
+	protected void onCreate(Bundle bun) {
+		super.onCreate(bun);
+		setContentView(R.layout.activity_game);
+		
+		bun = getIntent().getExtras();
+		max = bun.getInt("players");
+		
+		ThirtyOne game = new ThirtyOne(max);
+	}
+}
